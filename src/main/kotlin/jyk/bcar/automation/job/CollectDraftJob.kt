17ebe75a03ebf21acf85d99418a53878804ea701
wns312 <wns312@gmail.com>
2025-12-29 16:59:59 +0900

    feat: 로그인 동작 추가

--- src/main/kotlin/jyk/bcar/automation/job/CollectDraftJob.kt
+++ src/main/kotlin/jyk/bcar/automation/job/CollectDraftJob.kt
@@ -1,15 +1,23 @@
 package jyk.bcar.automation.job
 
+import com.microsoft.playwright.Page
 import jyk.bcar.automation.job.result.CollectDraftResult
 import jyk.bcar.automation.playwright.PlaywrightSessionRunner
-import kotlinx.coroutines.delay
+import jyk.bcar.domain.SourceAdminUser
+import jyk.bcar.repository.UserRepository
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class CollectDraftJob(
     private val runner: PlaywrightSessionRunner,
+    private val userRepository: UserRepository,
 ) : AutomationJob<CollectDraftResult> {
+    companion object {
+        const val COLLECT_LOGIN_URL = "http://thebestcar.kr/mypage/login.html"
+        const val COLLECT_ADMIN_URL = "http://thebestcar.kr/mypage/mycar.html"
+    }
+
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override val name: String = "collect-draft"
@@ -17,16 +25,31 @@ class CollectDraftJob(
     override suspend fun execute(): CollectDraftResult {
         logger.info("Collecting draft ids.")
 
+        val sourceAdminUser = userRepository.findSourceAdminUser()
         runner.withSession { session ->
-            session.usePage { page ->
-                page.navigate("https://google.com")
-                delay(10000)
+            session.usePage {
+                it.loginSourceAdminPage(sourceAdminUser)
             }
         }
+
+        logger.info("Login succeeded")
 
         return CollectDraftResult(
             draftIds = listOf("draft-1", "draft-2"),
             message = "drafts collected",
         )
+    }
+
+    private suspend fun Page.loginSourceAdminPage(sourceAdminUser: SourceAdminUser) {
+        this.navigate(COLLECT_LOGIN_URL)
+        check(this.url() == COLLECT_LOGIN_URL)
+
+        this.locator(".iptD").let { iptDs ->
+            iptDs.nth(0).fill(sourceAdminUser.id)
+            iptDs.nth(1).fill(sourceAdminUser.password)
+        }
+
+        this.locator("button[class=\"btn_login\"]").click()
+        check(this.url() == COLLECT_ADMIN_URL)
     }
 }
