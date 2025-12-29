a9a2e03dfa1b45b4e7b19ad57523fdf2ed59c09a
wns312 <wns312@gmail.com>
2025-12-29 17:22:33 +0900

    fix: ktlint

--- src/test/kotlin/jyk/bcar/BcarApplicationTests.kt
+++ src/test/kotlin/jyk/bcar/BcarApplicationTests.kt
@@ -1,8 +1,8 @@
 package jyk.bcar
 
+import com.google.api.services.sheets.v4.Sheets
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import com.google.api.services.sheets.v4.Sheets
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
